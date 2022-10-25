80fdf72aefcfba76a508e48cf288da681e943f74
Jisan-Hasan <jisan.jh5@gmail.com>
2022-10-25 18:22:39 +0600

    Home page added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,9 +1,46 @@
-import React from 'react';
+import React from "react";
+import { Button, Col, Container, Image, Row } from "react-bootstrap";
 
 const Home = () => {
     return (
-        <div>
-            <h2>This is Home</h2>
+        <div className="mt-5">
+            <Container>
+                <Row className="d-lg-flex justify-content-between align-items-center">
+                    <Col lg="6">
+                        <Image className="img-fluid w-75" src="robot-1.png" />
+                    </Col>
+                    <Col lg="6">
+                        <h1>Better Future Learn the Secrets Life Success.</h1>
+                        <p>
+                            Better future at volutpat diam ut. Purus sit amet
+                            luctus venenatis lectus magna dui sapien eget mi
+                            proin sed libero enim sed. Tempor orci eu lobortis
+                            elementum nibh tellus molestie at urna condimentum
+                            mattis pellentesque id nibh tortor id.
+                        </p>
+                        <div className="d-md-flex justify-content-between">
+                            <div>
+                                <h2>4.8</h2>
+                                <p>Rating Course</p>
+                            </div>
+                            <div>
+                                <h2>6+</h2>
+                                <p>Online Courses</p>
+                            </div>
+                            <div>
+                                <h2>15 k+</h2>
+                                <p>Students Enrolled</p>
+                            </div>
+                        </div>
+                        <div>
+                            <Button className="me-2" variant="primary">
+                                See Courses
+                            </Button>
+                            <Button variant="outline-dark">Login</Button>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
--- src/pages/Shared/Header/Header.js
+++ src/pages/Shared/Header/Header.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 const Header = () => {
     return (
-        <div>
-            <h2>This is header</h2>
-        </div>
+        <Navbar collapseOnSelect expand="lg" bg="primary" variant="dark">
+      <Container>
+        <Navbar.Brand href="#home">SkillMall</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#features">Courses</Nav.Link>
+            <Nav.Link href="#pricing">Blog</Nav.Link>
+            <Nav.Link href="#pricing">FAQ</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link href="#deets">Dark</Nav.Link>
+            <Nav.Link eventKey={2} href="#memes">
+              User
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
     );
 };
 
